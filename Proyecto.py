d5f73f5bd7435c2b2a25c8c9fedb9ac7b0bf3be3
Tomas <tomas.cornejo.y@usach.cl>
2025-06-21 02:25:31 -0400

    Se agrega la clase sensor

--- Proyecto.py
+++ Proyecto.py
@@ -27,3 +27,22 @@ class Luz:
     def apagar(self):
         self.estado = False
         print(f"[INFO] Luz {self.color.upper()} apagada.")
+
+class Sensor:
+    def __init__(self, nombre, valor_inicial=0):
+        self.nombre = nombre
+        self.valor = valor_inicial
+        self.activo = True
+
+    def leer(self):
+        if not self.activo:
+            raise Exception(f"[ERROR] Sensor {self.nombre} no responde.")
+        return self.valor
+    
+    def set_valor(self, valor):
+        self.valor = valor
+
+    def desactivar(self):
+        self.activo = False
+
+        
