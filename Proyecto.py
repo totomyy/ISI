5c3087e4fa201a78c5cf1e02131e79b14b1eadc1
Tomas <tomas.cornejo.y@usach.cl>
2025-06-21 02:14:05 -0400

    se agrega la clase luz

--- Proyecto.py
+++ Proyecto.py
@@ -14,3 +14,16 @@ class Motor:
     def desactivar(self):
         self.estado = False
         print(f"[INFO] Motor {self.nombre} desactivado.")
+
+class Luz:
+    def __init__(self, color):
+        self.color = color
+        self.estado = False
+
+    def encender(self):
+        self.estado = True
+        print(f"[INFO] Luz {self.color.upper()} encendida.")
+
+    def apagar(self):
+        self.estado = False
+        print(f"[INFO] Luz {self.color.upper()} apagada.")
