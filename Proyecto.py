e2f387c5e32e3a00829025bcf1f1500719ba6a1a
Tomas <tomas.cornejo.y@usach.cl>
2025-06-23 00:06:24 -0400

    Se añade una simulación para demostración practica

--- Proyecto.py
+++ Proyecto.py
@@ -179,3 +179,13 @@ class SistemaAcoplamiento:
         except Exception as e:
             print(f"[ERROR] Fallo al bajar rampa: {e}")
             return False
+
+
+# Ejecución simulada
+if input("¿Comenzar?\n") == "si":
+    camion = Camion("C-101", distancia_inicial=5, alineado_izq=True, alineado_der=True)
+    sistema = SistemaAcoplamiento()
+    sistema.sensor_izq.set_valor(True)
+    sistema.sensor_der.set_valor(True)
+    sistema.sensor_distancia.set_valor(camion.distancia)
+    sistema.acoplar_camion(camion)
